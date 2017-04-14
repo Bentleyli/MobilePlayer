f14c9c4f09b961aa210c9f241aa9dc06ab782e73
bentley <670259582@qq.com>
2017-04-14 17:06:20 +0800

    实现音乐播放和暂停

--- app/src/main/java/com/ljh/mobileplayer/activity/AudioPlayerActivity.java
+++ app/src/main/java/com/ljh/mobileplayer/activity/AudioPlayerActivity.java
@@ -1,28 +1,173 @@
 package com.ljh.mobileplayer.activity;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
+import com.ljh.mobileplayer.IMusicPlayerService;
 import com.ljh.mobileplayer.R;
+import com.ljh.mobileplayer.service.MusicPlayerService;
 
 /**
  * Created by Bentley on 2017/4/1.
  */
-public class AudioPlayerActivity extends Activity {
+public class AudioPlayerActivity extends Activity implements View.OnClickListener {
 
-    private ImageView iv_icon;
+    private int position;
+    private IMusicPlayerService service;//服务的代理类，通过它可以调用服务的方法
+
+    private ImageView ivIcon;
+    private TextView tvArtist;
+    private TextView tvName;
+    private TextView tvTime;
+    private SeekBar seekbarAudio;
+    private Button btnAudioPlaymode;
+    private Button btnAudioPre;
+    private Button btnAudioStartPause;
+    private Button btnAudioNext;
+    private Button btnLyric;
+
+    private ServiceConnection conn = new ServiceConnection() {
+
+        /**
+         * 当连接成功的时候回调这个方法
+         * @param name
+         * @param iBinder
+         */
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder iBinder) {
+            service = IMusicPlayerService.Stub.asInterface(iBinder);
+            if (service != null) {
+                try {
+                    service.openAudio(position);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * 当连接断开的时候回调这个方法
+         * @param name
+         */
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            try {
+                if (service != null) {
+                    service.stop();
+                    service = null;
+                }
+
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    /**
+     * Find the Views in the layout<br />
+     * <br />
+     * Auto-created on 2017-04-14 15:18:21 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    private void findViews() {
+        setContentView(R.layout.activity_audioplayer);
+        ivIcon = (ImageView) findViewById(R.id.iv_icon);
+        ivIcon.setBackgroundResource(R.drawable.animation_list);
+        AnimationDrawable rocketAnimation = (AnimationDrawable) ivIcon.getBackground();
+        rocketAnimation.start();
+
+        tvArtist = (TextView) findViewById(R.id.tv_artist);
+        tvName = (TextView) findViewById(R.id.tv_name);
+        tvTime = (TextView) findViewById(R.id.tv_time);
+        seekbarAudio = (SeekBar) findViewById(R.id.seekbar_audio);
+        btnAudioPlaymode = (Button) findViewById(R.id.btn_audio_playmode);
+        btnAudioPre = (Button) findViewById(R.id.btn_audio_pre);
+        btnAudioStartPause = (Button) findViewById(R.id.btn_audio_start_pause);
+        btnAudioNext = (Button) findViewById(R.id.btn_audio_next);
+        btnLyric = (Button) findViewById(R.id.btn_lyric);
+
+        btnAudioPlaymode.setOnClickListener(this);
+        btnAudioPre.setOnClickListener(this);
+        btnAudioStartPause.setOnClickListener(this);
+        btnAudioNext.setOnClickListener(this);
+        btnLyric.setOnClickListener(this);
+    }
+
+    /**
+     * Handle button click events<br />
+     * <br />
+     * Auto-created on 2017-04-14 15:18:21 by Android Layout Finder
+     * (http://www.buzzingandroid.com/tools/android-layout-finder)
+     */
+    @Override
+    public void onClick(View v) {
+        if (v == btnAudioPlaymode) {
+            // Handle clicks for btnAudioPlaymode
+        } else if (v == btnAudioPre) {
+            // Handle clicks for btnAudioPre
+        } else if (v == btnAudioStartPause) {
+            // Handle clicks for btnAudioStartPause
+
+            if (service != null) {
+                try {
+                    if (service.isPlaying()) {
+                        //暂停
+                        service.pause();
+                        //按钮——播放
+                        btnAudioStartPause.setBackgroundResource(R.drawable.btn_audio_start_selector);
+                    }else {
+                        //播放
+                        service.start();
+                        //按钮——暂停
+                        btnAudioStartPause.setBackgroundResource(R.drawable.btn_audio_pause_selector);
+                    }
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if (v == btnAudioNext) {
+            // Handle clicks for btnAudioNext
+        } else if (v == btnLyric) {
+            // Handle clicks for btnLyric
+        }
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_audioplayer);
-        iv_icon = (ImageView) findViewById(R.id.iv_icon);
-        iv_icon.setBackgroundResource(R.drawable.animation_list);
+        initView();
+        findViews();
+        getData();
+        bindAndStartService();
+    }
 
-        AnimationDrawable rocketAnimation = (AnimationDrawable) iv_icon.getBackground();
-        rocketAnimation.start();
+
+    private void bindAndStartService() {
+        Intent intent = new Intent(this, MusicPlayerService.class);
+        intent.setAction("com.ljh.mobileplayer_OPENAUDIO");
+        bindService(intent, conn, Context.BIND_AUTO_CREATE);
+        startService(intent);//不至于实例化多个服务
+    }
+
+    //得到数据
+    private void getData() {
+        position = getIntent().getIntExtra("position", 0);
+    }
+
+    private void initView() {
+
     }
 }
